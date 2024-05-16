a1e9903b193f9a7e2ebffeb9a76b123d972930ef
Yuriy <russian_town_98@mail.ru>
2024-05-16 22:52:17 +0600

    add_first_colume_find_match

--- Assets/Source/Finder/MatchFinder.cs
+++ Assets/Source/Finder/MatchFinder.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Sourse.Candies;
 using Sourse.Configs;
 using Sourse.Constants;
+using Sourse.GameboardContent;
 using Sourse.GameboardContent.CellContent;
 using UnityEngine;
 
@@ -11,35 +12,41 @@ namespace Sourse.Finder
     {
         private List<Cell> _cells = new ();
         private GameboardConfig _gameboardConfig;
+        private Gameboard _gameboard;
 
-        public MatchFinder(List<Cell> cells, GameboardConfig gameboardConfig)
+        public MatchFinder(
+            List<Cell> cells,
+            GameboardConfig gameboardConfig,
+            Gameboard gameboard)
         {
             _cells = cells;
             _gameboardConfig = gameboardConfig;
+            _gameboard = gameboard;
         }
 
         public bool HasMatch(out List<Candy> matches)
         {
-            List<Candy> candies = new ();
+            List<Candy> temp = new ();
 
             for (int i = 0; i < _gameboardConfig.Width; i++)
             {
-                for (int j = 0; j < _gameboardConfig.Height; j++)
-                {
-                    if (_cells[i].CandyType == _cells[j].CandyType)
-                    {
-                        candies.Add(_cells[i].Candy);
-                        candies.Add(_cells[j].Candy);
-                        Debug.Log("poop");
-                    }
+                if (i + 1 == _cells.Count - 1)
+                    break;
 
-                    if(candies.Count >= GameParameter.MinCandyCountToMatch)
+                if (_cells[i].CandyType == _cells[i + 1].CandyType)
+                {
+                    temp.Add(_cells[i].Candy);
+                    temp.Add(_cells[i + 1].Candy);
+                }
+                else
+                {
+                    if (temp.Count >= GameParameter.MinCandyCountToMatch)
                     {
-                        matches = candies;
-                        candies.Clear ();
-                        Debug.Log(matches.Count);
+                        matches = temp;
                         return true;
                     }
+
+                    temp.Clear();
                 }
             }
 
